70d0e97536c1f500c255a151dd4de07f57c0b71d
Yugsolanki <yugsolanki15@gmail.com>
2022-06-25 12:04:06 +0530

    till player.html

---
+++ src/main.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+import Highway from '@dogstudio/highway';
+
+// Import Transitions
+import Fade from './fade.js';
+import Overlap from './overlap.js';
+
+// Call Highway.Core once.
+const H = new Highway.Core({
+  transitions: {
+    default: Fade,
+    contextual: {
+      overlap: Overlap
+    }
+  }
+});
